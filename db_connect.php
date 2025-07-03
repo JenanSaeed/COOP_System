946e43578938ba944ecfab416ff51836b548f2c6
Walaa <walaasalbahrani@gmail.com>
2025-07-03 08:33:50 +0300

    Update db_connect.php
    Add database name

--- db_connect.php
+++ db_connect.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $username = "root";
 $password = "";
-$database = "";
+$database = "coop_db";
 $conn = mysqli_connect($host, $username, $password, $database);
 //or die("Connection failed"); will be added later
 if(!$conn){
