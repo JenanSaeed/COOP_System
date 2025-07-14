42214bcd486afaaedbe3640bd86fa1574925243e
Walaa <walaasalbahrani@gmail.com>
2025-07-14 11:11:11 +0300

    Update login.php

--- login.php
+++ login.php
@@ -85,7 +85,9 @@ if (isset($_GET['error'])) {
     <label class="loglabels" for="password">كلمة المرور:</label>
     <input class="loginputs" type="password" id="password" name="password" placeholder="كلمة المرور" required>
 
-    <input class="buttons" type="submit" value="تسجيل الدخول">
+    <div class="form-buttons">
+        <button class="buttons" type="submit" value="تسجيل الدخول">تسجيل الدخول</button>
+    </div>
   </form>
 </div>
 
