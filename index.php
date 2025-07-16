44b7aabd4b928fc6a85ab2d35d2b9e0c71e19731
Raneem Alturki <raneem.turki@gmail.com>
2025-07-16 11:02:31 +0300

    Update index.php

--- index.php
+++ index.php
@@ -20,15 +20,15 @@
 
     switch ($role) {
       case 'employee':
-        $contractsLink = "contractMain.php";
+        $contractsLink = "c-main.php";
         $vacationsLink = "empMain.php";
         break;
       case 'finance':
-        $contractsLink = "contractMain.php";
+        $contractsLink = "c-main.php";
         $vacationsLink = "finMain.php";
         break;
       case 'manager':
-        $contractsLink = "contractMain.php";
+        $contractsLink = "c-main.php";
         $vacationsLink = "manMain.php";
         break;
     }
