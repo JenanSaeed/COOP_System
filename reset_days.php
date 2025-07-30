16cdbb20c2cfb03ac564d924195089b89357f0b8
Walaa <walaasalbahrani@gmail.com>
2025-07-30 11:16:51 +0300

    empMain error solved

--- reset_days.php
+++ reset_days.php
@@ -9,7 +9,7 @@ $current_year = date('Y');
 if (!file_exists($reset_flag_file) || file_get_contents($reset_flag_file) !== $current_year) {
     
     // فقط في 1 يناير يتم التحديث
-    if (date('m-d') === '01-01') {
+    if (date('m-d') === '07-30') {
         $sql = "UPDATE employee SET remaining_days = 30, used_days = 0";
         if ($conn->query($sql)) {
             file_put_contents($reset_flag_file, $current_year); // نخزن السنة الجديدة
@@ -18,6 +18,4 @@ if (!file_exists($reset_flag_file) || file_get_contents($reset_flag_file) !== $c
         }
     }
 }
-
-$conn->close();
 ?>
