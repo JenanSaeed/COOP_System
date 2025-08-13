58132d1786f548d3dc4648f96bff54c79b3bea7a
fatimahj3 <fatemah36618@gmail.com>
2025-08-13 13:04:51 +0300

    Update c-guestSign.php

--- c-guestSign.php
+++ c-guestSign.php
@@ -52,18 +52,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $destPath = $uploadDir . 'sign.png'; // Always save as 'sign.png'
 
     if (move_uploaded_file($fileTmpPath, $destPath)) {
-        // ✅ تحويل الصورة PNG إلى JPEG (لإزالة الشفافية)
-            $image = imagecreatefrompng($destPathPng);
-            if ($image) {
-                $bg = imagecreatetruecolor(imagesx($image), imagesy($image));
-                $white = imagecolorallocate($bg, 255, 255, 255);
-                imagefilledrectangle($bg, 0, 0, imagesx($image), imagesy($image), $white);
-                imagecopy($bg, $image, 0, 0, 0, 0, imagesx($image), imagesy($image));
-                imagejpeg($bg, $destPathJpg, 100);
-                imagedestroy($image);
-                imagedestroy($bg);
-                unlink($destPathPng); // حذف نسخة PNG
-            }
       // Prepare data from POST
       $con_id = $_POST['con_id'];
       $id_number = $_POST['id_number'];
@@ -130,7 +118,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   <link rel="stylesheet" href="style.css">
   <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@400;700&display=swap" rel="stylesheet">
 </head>
-<body class="bg-light">
+<body>
 
 <div class="GScontainer">
   <form method="POST" enctype="multipart/form-data" class="GSform">
